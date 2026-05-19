4120e630d0bdaad5be838a89b58496271f048d84
bizhar-cloud <bjar.bergarey@gmail.com>
2026-05-20 02:25:43 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dama-v1';
+const CACHE_NAME = 'dama-v2'; // ناڤێ کاشێ هاتە گۆهۆڕین بۆ v2 دا کو گۆهۆڕینێن نوی ب کار بکەڤن
 const urlsToCache = [
   './',
   './index.html',
@@ -7,6 +7,7 @@ const urlsToCache = [
   './manifest.json'
 ];
 
+// دابەزاندنا فایلان د ناڤ مۆبایلێ دا (Install)
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -14,8 +15,28 @@ self.addEventListener('install', event => {
         return cache.addAll(urlsToCache);
       })
   );
+  // ڕاستەوخۆ ڤێرژنێ نوی بخە کار
+  self.skipWaiting();
 });
 
+// ژێبرنا فایلێن کەڤن ئەگەر ڤێرژنەکێ نوی هات (Activate)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  // کۆنترۆلا پەیچی ڕاستەوخۆ وەرگرە
+  self.clients.claim();
+});
+
+// بکارئینانا فایلان ژ مۆبایلێ (Fetch) بەری ئینتەرنێتێ
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
@@ -28,4 +49,3 @@ self.addEventListener('fetch', event => {
     )
   );
 });
-
